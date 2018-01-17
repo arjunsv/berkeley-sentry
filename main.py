13b0d6ac03b524f413561c43a0e336441c851b03
Arjun Vasudevan <arjunsv@berkeley.edu>
2018-01-17 00:01:50 -0800

    able to map most recent calls and place circles in location using municipal api

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
-from zillowAPI import zillow
 import requests
-
-z = zillow()
-api_key = "X1-ZWz1g6p3pcqpsb_94p25"
-zestimate_result = z.GetZestimate(api_key, zpid=48749425, rentzestimate=True)
-
-# notes
-# [lon, lat, bedrooms, bathrooms, ]
-
-
 r = requests.get('https://data.cityofberkeley.info/resource/s24d-wsnp.json')
